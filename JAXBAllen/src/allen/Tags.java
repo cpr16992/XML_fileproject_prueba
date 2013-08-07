fc42ea727def3faa049e47d72540a067eb0d986f
invitado <invitado@c3po.lim.local>
2013-08-07 18:48:07 +0200

    Solucionado problema con el path

--- JAXBAllen/src/allen/Tags.java
+++ JAXBAllen/src/allen/Tags.java
@@ -1,5 +1,8 @@
 package allen;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -7,27 +10,29 @@ import javax.xml.bind.Unmarshaller;
 public class Tags {
 
 
-	public static void main(String[] args) {    
+	public static void main(String[] args) {
 		try {
 			final JAXBContext jc = JAXBContext.newInstance("allen");
 			final Unmarshaller u = jc.createUnmarshaller();
-			
-			
+
+
 			final Response.Structure rootElem = extracted(u);
 			System.out.println(rootElem.getName());
 			System.out.println(rootElem.getChildren().getChild());
-			
+
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 
 	}
 
-	private static Response.Structure extracted(final Unmarshaller u) throws JAXBException
+	private static Response.Structure extracted(final Unmarshaller u) throws Exception
 	{
-		 Response elm = (Response)u.unmarshal(Tags.class.getResourceAsStream("ref.xml"));
-			return elm.getStructure();
+		InputStream is = new FileInputStream("C:/Documents and Settings/invitado/git/XML_fileproject/.project/JAXBAllen/src/ref.xml");
+		//InputStream is = new FileInputStream("C:\\Documents and Settings\\invitado\\git\\XML_fileproject\\.project\\JAXBP\\src\\org\\example\\schemabook\\Book.xml");
+		Response elm = (Response)u.unmarshal(is);
+		return elm.getStructure();
 	}
 }
